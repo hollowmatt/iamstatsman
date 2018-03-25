5087d289584d78083b68873d8c6a1fe6cc188c06
Matt Holloway <hollowmatt@gmail.com>
2018-03-25 09:30:30 -0400

    Added validators to team, further specs

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,4 @@
 class Team < ApplicationRecord
+	validates :name, presence: true, uniqueness: true
+	validates :sdm, presence: true
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -5,4 +5,14 @@ describe Team do
 		team = create(:team)
 		expect(team).to be_valid
 	end
+
+	it "is invadlid without a team name" do 
+		expect(FactoryBot.build(:team, name: nil)).to_not be_valid
+	end
+
+	it "is invalid with a duplicate team name" do 
+		team = create(:team)
+		expect(team).to be_valid
+		expect(FactoryBot.build(:team)).to_not be_valid
+	end
 end
