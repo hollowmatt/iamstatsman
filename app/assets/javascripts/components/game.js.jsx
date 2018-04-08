81a924b48221480acbd3e551a4e8457b902a19ca
Matt Holloway <hollowmatt@gmail.com>
2018-04-08 10:30:06 -0400

    Setup something new, to see if I can get the full page thing to work

---
+++ app/assets/javascripts/components/game.js.jsx
@@ -0,0 +1,13 @@
+class Game extends React.Component {
+  constructor(props) { 
+    super(props);
+	} 
+
+  render() {
+    return (
+      <div>
+        <p>Tic Tac Toe</p>
+      </div>
+    );
+  }
+}
