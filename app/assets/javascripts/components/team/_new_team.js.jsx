9b9cd891d7b94b86d724ec65a3c1c79a336e1568
Matt Holloway <hollowmatt@gmail.com>
2018-04-20 19:38:24 -0400

    Ok, fix this tomorrow... gotta push and run

--- app/assets/javascripts/components/team/_new_team.js.jsx
+++ app/assets/javascripts/components/team/_new_team.js.jsx
@@ -47,42 +47,36 @@ class NewTeam extends React.Component{
         	<input type="text" 
         			   name="name" 
         			   placeholder='Enter the team name'
-        			   value={this.props.newTeam.name}
         			   onChange={this.handleChange} />
         </div>
       	<div className="form-group">
         	<input type="text" 
         				 name="sdm" 
         				 placeholder='Enter the name of the team manager'
-        			   value={this.props.newTeam.sdm}
         			   onChange={this.handleChange} />
         </div>
       	<div className="form-group">	
         	<input type="text" 
         				 name="techlead" 
         				 placeholder='Enter the name of the tech lead'
-        			   value={this.props.newTeam.techlead}
         			   onChange={this.handleChange} />
         </div>
       	<div className="form-group">
         	<input type="text" 
         				 name="tpm" 
         				 placeholder='Enter the name of the TPM'
-        			   value={this.props.newTeam.tpm}
         			   onChange={this.handleChange} />
         </div>
       	<div className="form-group">
         	<input type="text" 
         				 name="pm" 
         				 placeholder='Enter the name of the Product Manager'
-        			   value={this.props.newTeam.pm}
         			   onChange={this.handleChange} />
         </div>
       	<div className="form-group">
         	<input type="text" 
         				 name="sm" 
         				 placeholder='Enter the name of the Scrum Master'
-        			   value={this.props.newTeam.sm}
         			   onChange={this.handleChange} />
         </div>
       	<button onClick={()=> this.handleAdd(e)} className="btn flatt-butt-md flat-primary-butt">Save</button>
