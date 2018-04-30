df30c335cbcd7d49888461994320f43c37462dc1
Matt Holloway <hollowmatt@gmail.com>
2018-04-29 21:07:13 -0400

    Break out table header

--- app/assets/javascripts/components/squad/_all_squads.js.jsx
+++ app/assets/javascripts/components/squad/_all_squads.js.jsx
@@ -8,38 +8,7 @@ class AllSquads extends React.Component {
 
 		return (
 			<div>
-				<div className="row">
-					<div className="col s2">
-						<strong>
-							Team Name
-						</strong>
-					</div>
-					<div className="col s2">
-						<strong>
-							SDM
-						</strong>
-					</div>
-					<div className="col s2">
-						<strong>
-							Tech Lead
-						</strong>
-					</div>
-					<div className="col s2">
-						<strong>
-							TPM
-						</strong>
-					</div>
-					<div className="col s2">
-						<strong>
-							Product
-						</strong>
-					</div>
-					<div className="col s2">
-						<strong>
-							Scrum Master
-						</strong>
-					</div>
-				</div>
+				<SquadTableHeader />
 				{teams}
 			</div>
 		);
---
+++ app/assets/javascripts/components/squad/_squad_table_header.js.jsx
@@ -0,0 +1,38 @@
+class SquadTableHeader extends React.Component {
+	render() {
+		return(
+			<div className="row">
+				<div className="col s2">
+					<strong>
+						Team Name
+					</strong>
+				</div>
+				<div className="col s2">
+					<strong>
+						SDM
+					</strong>
+				</div>
+				<div className="col s2">
+					<strong>
+						Tech Lead
+					</strong>
+				</div>
+				<div className="col s2">
+					<strong>
+						TPM
+					</strong>
+				</div>
+				<div className="col s2">
+					<strong>
+						Product
+					</strong>
+				</div>
+				<div className="col s2">
+					<strong>
+						Scrum Master
+					</strong>
+				</div>
+			</div>
+		);
+	}
+}
