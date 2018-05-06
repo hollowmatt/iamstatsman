90034b21d836c025871ebc303586cb4662b086a6
Matt Holloway <hollowmatt@gmail.com>
2018-05-06 09:05:07 -0400

    Working more on the edit function

--- app/assets/javascripts/components/squad/_squad.js.jsx
+++ app/assets/javascripts/components/squad/_squad.js.jsx
@@ -1,23 +1,30 @@
 class Squad extends React.Component {
-	
-	handleUpdate(e) {
+	constructor(props) {
+		super(props);
+		this.state = {
+			edit: false,
+		};
+	}
+	handleEdit(e) {
     e.preventDefault();
     if (this.validRecord()) {
       var event_data = {
-        name: this.recordValue("name"),
-        description: this.recordValue("description"),
-        date: this.recordValue("date"),
-        place: this.recordValue("place")
+      	name: this.recordValue("name"),
+      	sdm: this.recordValue("sdm"),
+      	techlead: this.recordValue("techlead"),
+      	tpm: this.recordValue("tpm"),
+      	pm: this.recordValue("pm"),
+      	sm: this.recordValue("sm")
       };
       $.ajax({
-        method: 'PUT',
-        url: '/api/events/' + this.props.event.id,
+      	method: 'PUT',
+        url: '/api/v1/teams' + this.props.team_id,
         data: { event: event_data },
-        success: function(data) {
-          this.props.handleUpdateRecord(this.props.event, data);
+        success: (data) => {
+          this.props.handleUpdateRecord(this.props.team_id, data);
           this.setState({ edit: false });
         }.bind(this),
-        error: function(xhr, status, error) {
+        error: (xhr, status, error) => {
           alert('Cannot update requested record: ', error);
         }
       });
@@ -26,7 +33,39 @@ class Squad extends React.Component {
     }
   }
 
-	render() {
+  handleToggle(e) {
+    e.preventDefault();
+    this.setState({ edit: !this.state.edit });
+  }
+
+  validRecord() {
+    if (this.state.name && this.state.sdm && this.state.techlead &&
+        this.state.tpm && this.state.pm && this.state.sm ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  handleChange(e) {
+    var input_name = e.target.name;
+    var value = e.target.value;
+    this.setState({[input_name] : value });
+  }
+
+  render() {
+  	if(this.state.edit) {
+  		return(this.renderForm());
+  	} else {
+  		return(this.renderData());
+  	}
+  }
+
+  renderForm() {
+
+  }
+
+	renderData() {
 		var team = this.props.team
 		var idx = this.props.idx
 		return(
