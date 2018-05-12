230ad18bb7f447ea75e863578730a7a5bfdd6dde
Matt Holloway <hollowmatt@gmail.com>
2018-05-12 14:05:11 -0400

    Fixed database migration

--- db/migrate/20180512025620_create_peeps.rb
+++ db/migrate/20180512025620_create_peeps.rb
@@ -1,8 +1,8 @@
 class CreatePeeps < ActiveRecord::Migration[5.1]
   def change
     create_table :peeps do |t|
-      t.first_name :string
-      t.last_name :string
+      t.string :first_name
+      t.string :last_name
 
       t.timestamps
     end
