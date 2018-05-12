918660e292fc05f10b622b0dc5ca1a5c8749b10b
Matt Holloway <hollowmatt@gmail.com>
2018-05-12 14:06:29 -0400

    update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180325233413) do
+ActiveRecord::Schema.define(version: 20180512025620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "peeps", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "siteperves", force: :cascade do |t|
     t.string "pagename"
