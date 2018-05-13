c1af668d2314d3df9a84dbc488e4f945fec8391d
Matt Holloway <hollowmatt@gmail.com>
2018-05-13 11:42:29 -0400

    Update routes for new API

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
   namespace :api do
   	namespace :v1 do
   		resources :teams, only: [:index, :create, :destroy, :update, :show]
-  	end
+  	  resources :peeps, only: [:index, :create, :destroy, :update, :show]
+    end
   end
 
 end
